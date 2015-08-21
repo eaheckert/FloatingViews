62b1240a1aac4f8e3c864eca670f4cfe85249122
Eugene Heckert <eaheckert@gmail.com>
2015-08-20 23:42:21 -0500

    * now displaying floating views when FloatingViewController appears on screen.

--- FloatingView/FloatingView/FloatingView/FloatingView.swift
+++ FloatingView/FloatingView/FloatingView/FloatingView.swift
@@ -16,6 +16,7 @@ class FloatingView: UIView
     //MARK: IB Varaibles
     
     @IBOutlet var mainView: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
     
     
     //MARK: Required Methods
@@ -34,5 +35,31 @@ class FloatingView: UIView
         NSBundle.mainBundle().loadNibNamed("FloatingView", owner: self, options: nil)
         self.addSubview(self.mainView)
         self.frame = self.mainView.frame
+    }
+    
+    override var frame: CGRect
+    {
+        get
+        {
+            return super.frame
+        }
+        set(newFrame)
+        {
+            super.frame = newFrame
+            
+            
+            if (mainView != nil)
+            {
+                var frame = newFrame
+                frame.origin = CGPointZero
+                mainView.frame = frame
+            }
+            
+        }
+    }
+    
+    func loadMessageLabel(message: String)
+    {
+        messageLabel.text = message
     }
 }
--- FloatingView/FloatingView/FloatingView/FloatingViewController.swift
+++ FloatingView/FloatingView/FloatingView/FloatingViewController.swift
@@ -18,6 +18,26 @@ class FloatingViewController: UIViewController
         // Do any additional setup after loading the view, typically from a nib.
         
         self.view.backgroundColor = UIColor(white: 0.0, alpha: 0.5)
+        
+        var messageAr: Array = ["Go for a run", "take a walk", "make dinner", "hang out with friends"]
+        
+        var xPos: CGFloat = 0.0
+        var yPos: CGFloat = 20.0
+        
+        for message in messageAr
+        {
+            var messageView: FloatingView = FloatingView()
+            
+            messageView.loadMessageLabel(message)
+            
+            messageView.frame = CGRect(x: xPos, y: yPos, width: messageView.frame.width, height: messageView.frame.height)
+            
+            xPos += messageView.frame.width/2
+            
+            yPos += messageView.frame.height
+            
+            view.addSubview(messageView)
+        }
     }
     
     override func didReceiveMemoryWarning()
